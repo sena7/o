81394cdae243951a44e7bf46620b5cbb4b36b765
qhb16114 <qhb16114@ADM-SIM-PC034.mis.strath.ac.uk>
2017-10-27 16:32:58 +0100

    201710271632

--- o/src/time/Time.java
+++ o/src/time/Time.java
@@ -2,12 +2,15 @@ package time;
 
 import java.time.Duration;
 import java.time.ZonedDateTime;
+import java.time.temporal.TemporalUnit;
 
 public class Time {
 
 	private ZonedDateTime startTime;
 	private ZonedDateTime endTime;
 	private Duration duration;
+	// TODO unit would be only parameterised for view methods.
+	private TemporalUnit unit;
 
 	// Accessors
 	public ZonedDateTime getStartTime() {
@@ -16,35 +19,39 @@ public class Time {
 
 	public void setStartTime(ZonedDateTime startTime) {
 		this.startTime = startTime;
-		//if duration has value, set the endTime.
-		if(this.duration!=null) {
-			setEndTime(this.startTime, this.duration);
+		
+		// if duration has value, set the endTime.
+		if (this.duration != null) {
+			calculateEndTime(this.startTime, this.duration);
 		}
 	}
 
 	public ZonedDateTime getEndTime() {
 		return endTime;
 	}
-/**
- * 
- * @param endTime
- * Duration will be adjusted or assigned if this startTime is not null.
- */
+
+	/**
+	 * 
+	 * @param endTime
+	 *            Duration will be adjusted or assigned if this startTime is not
+	 *            null.
+	 */
 	public void setEndTime(ZonedDateTime endTime) {
 		this.endTime = endTime;
+
 		
-		//if this startTime is not null, set this duration. 
-		if(this.startTime!=null) {
-			setDuration(this.startTime, this.endTime); 
-			
-		}else {
-		   //if this startTime is null but duration is not, set the startTime
-		   if(this.duration!=null) {
-			   setStartTime(this.endTime, this.duration);
-		   }//ELSE do nothing
-		   
+		// if this startTime is not null, set this duration.
+		if (this.startTime != null) {
+			calculateDuration(this.startTime, this.endTime);
+
+		} else {
+			// if this startTime is null but duration is not, set the startTime
+			if (this.duration != null) {
+				calculateStartTime(this.endTime, this.duration);
+			} // ELSE do nothing
+
 		}
-		
+
 	}
 
 	public Duration getDuration() {
@@ -54,20 +61,50 @@ public class Time {
 	public void setDuration(Duration duration) {
 		this.duration = duration;
 	}
-	
 
-	// overloaded
-	public void setStartTime(ZonedDateTime endTime, Duration duration) {
-		//TODO 
+	public TemporalUnit getUnit() {
+		return unit;
 	}
 
-	public void setDuration(ZonedDateTime startTime, ZonedDateTime endTime) {
-		//TODO
+	public void setUnit(TemporalUnit unit) {
+		this.unit = unit;
 	}
 
-	public void setEndTime(ZonedDateTime startTime, Duration duration) {
-		//TODO
+	// overloaded methods
 
+	/**
+	 * 
+	 * @param endTime
+	 * @param duration
+	 */
+	public void calculateStartTime(ZonedDateTime endTime, Duration duration) {
+		this.startTime = endTime.minus(duration);
+
+	}
+
+	/**
+	 * 
+	 * @param startTime
+	 * @param endTime
+	 */
+	public void calculateDuration(ZonedDateTime startTime, ZonedDateTime endTime) {
+		this.duration = Duration.between(this.startTime, this.endTime);
+	}
+
+	/**
+	 * 
+	 * @param startTime
+	 * @param duration
+	 */
+	public void calculateEndTime(ZonedDateTime startTime, Duration duration) {
+		this.endTime = startTime.plus(duration);
+
+	}
+
+	// TODO Define the use
+	// Do I even need this ?
+	public static enum DurationUnit {
+		MINUTES, SECONDS, NANOS;
 	}
 
 }
